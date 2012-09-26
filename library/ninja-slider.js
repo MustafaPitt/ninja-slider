b4ca82b38149d8b81474e986b2160a478b175cdb
Jamie Hoover ⚔ <dont.tase@me.com>
2012-09-26 14:26:09 -0700

    Initial Commit
    First working version of slider as a module.

---
+++ library/ninja-slider.js
@@ -0,0 +1,177 @@
+(function ($) {
+  'use strict';
+
+  $.Ninja.Slider = function (input, options) {
+    var slider = this;
+
+    if (input) {
+      slider.$input = $(input);
+
+      if (!slider.$input.is('input') || slider.$input.attr('type') !== 'hidden') {
+        $.ninja.error('Slider may only be called with a hidden <input> input.');
+      }
+    } else {
+      $.ninja.error('Slider must include a hidden <input> input.');
+    }
+
+    if (options) {
+      if ('list' in options) {
+        slider.list = options.list;
+
+        slider.slots = slider.list.length - 1;
+      } else {
+        $.ninja.error('Slider must include a list option.');
+      }
+
+      if ('select' in options && $.isFunction(options.select)) {
+        slider.select = options.select;
+      }
+
+      if (slider.$input.attr('value')) {
+        slider.index = $.inArray(slider.$input.val(), slider.list);
+      } else {
+        slider.$input.val(slider.list[0]);
+        slider.index = 0;
+      }
+    } else {
+      $.ninja.error('Slider called without options.');
+    }
+
+    if (slider.$input.parent().is('label')) {
+      slider.$wrapper = slider.$input.parent().addClass('ninja-slider');
+    } else {
+      slider.$wrapper = slider.$input.wrap('<label class="ninja-slider">').parent();
+    }
+
+    slider.width = slider.$wrapper.outerWidth();
+
+    slider.increment = slider.width / slider.slots;
+
+    slider.$level = $('<div>', {
+      'class': 'ninja-level',
+      css: {
+        width: slider.left()
+      }
+    });
+
+    slider.$groove = $('<div class="ninja-groove">').on('click.ninja', function (event) {
+      slider.$button.trigger({
+        type: 'select.ninja',
+        sliderX: Math.round((event.pageX - slider.$track.offset().left) / slider.increment)
+      });
+    }).append(slider.$level);
+
+    slider.$value = $('<span>', {
+      'class': 'ninja-value',
+      html: slider.list[slider.index]
+    });
+
+    slider.$button = $('<button>', {
+      'class': 'ninja-button',
+      css: {
+        left: slider.left()
+      }
+    }).on({
+      'keyup.ninja': function (event) {
+        var keycode = event.which;
+
+        if ($.ninja.key(keycode, ['arrowLeft', 'arrowRight'])) {
+          if (keycode === $.ninja.keys.arrowLeft) {
+            slider.move(slider.index - 1);
+          } else if (keycode === $.ninja.keys.arrowRight) {
+            slider.move(slider.index + 1);
+          }
+
+          slider.change();
+        }
+      },
+      'mousedown.ninja': function (event) {
+        slider.offsetX = event.pageX - Math.round(slider.$button.position().left);
+
+        $(document).on({
+          'mousemove.ninja': function (event) {
+            slider.move(Math.round((event.pageX - slider.offsetX) / slider.increment));
+          },
+          'mouseup.ninja': function () {
+            slider.change();
+
+            $(document).off('mousemove.ninja mouseup.ninja');
+          }
+        });
+      },
+      'touchstart.ninja': function (event) {
+        slider.$button.addClass('ninja-active');
+
+        slider.touch = event.originalEvent.targetTouches[0] || event.originalEvent.changedTouches[0];
+
+        slider.offsetX = slider.touch.pageX - Math.round(slider.$button.position().left);
+      },
+      'touchmove.ninja': function (event) {
+        event.preventDefault();
+
+        slider.touch = event.originalEvent.targetTouches[0] || event.originalEvent.changedTouches[0];
+
+        slider.move(Math.round((slider.touch.pageX - slider.offsetX) / slider.increment));
+      },
+      'touchend.ninja': function (event) {
+        event.preventDefault();
+
+        slider.$button.removeClass('ninja-active');
+
+        slider.change();
+      }
+    });
+
+    slider.$track = $('<div>', {
+      'class': 'ninja-track',
+      css: {
+        width: slider.width + 16
+      }
+    }).append(slider.$groove, slider.$button);
+
+    slider.$wrapper.append(slider.$value, slider.$track);
+
+    slider.drag = false;
+
+    slider.offsetX = 0;
+
+    slider.$input.data('ninja', {
+      slider: options
+    });
+  };
+
+  $.Ninja.Slider.prototype.change = function () {
+    this.$input.val(this.list[this.index]);
+  };
+
+  $.Ninja.Slider.prototype.left = function () {
+    return Math.round(this.index * this.increment);
+  };
+
+  $.Ninja.Slider.prototype.move = function (index) {
+    if (index !== this.index) {
+      if (index < 0) {
+        this.index = 0;
+      } else if (index > this.slots) {
+        this.index = this.slots;
+      } else {
+        this.index = index;
+      }
+
+      this.$button.css('left', this.left());
+      this.$level.css('width', this.left());
+      this.$value.text(this.list[this.index]);
+    }
+  };
+
+  $.ninja.slider = function (input, options) {
+    var $input = $(input);
+
+    if ($input.data('ninja') && 'slider' in $input.data('ninja')) {
+      $.ninja.warn('Slider called on the same input multiple times.');
+    } else {
+      $.extend(new $.Ninja(input, options), new $.Ninja.Slider(input, options));
+    }
+  };
+
+}(jQuery));
---
+++ test/ninja-slider.js
@@ -0,0 +1,166 @@
+(function ($) {
+  'use strict';
+
+  /*
+    ======== A Handy Little QUnit Reference ========
+    http://docs.jquery.com/QUnit
+
+    Test methods:
+      expect(numAssertions)
+      stop(increment)
+      start(decrement)
+    Test assertions:
+      ok(value, [message])
+      equal(actual, expected, [message])
+      notEqual(actual, expected, [message])
+      deepEqual(actual, expected, [message])
+      notDeepEqual(actual, expected, [message])
+      strictEqual(actual, expected, [message])
+      notStrictEqual(actual, expected, [message])
+      raises(block, [expected], [message])
+  */
+
+  var
+    list = [
+      '0 dB - silence',
+      '10 dB - calm breathing',
+      '20-30 dB - very calm room',
+      '40-60 dB - normal conversation',
+      '60 dB - TV set at home level',
+      '60-80 dB - Passenger car',
+      '78 dB - hearing damage over time',
+      '80-90 dB - traffic on a busy roadway',
+      '100 dB - jack hammer',
+      '120 dB - hearing damage immediately',
+      '130 dB - threshold of pain',
+      '150 dB - jet engine',
+      '168 dB - M1 Garand rifle'
+    ];
+
+  module('constructor', {
+    setup: function () {
+      this.constructor = $.Ninja.Slider;
+    }
+  });
+
+  test('exists', function () {
+    ok(this.constructor, 'should exist');
+  });
+
+  test('is a function', function () {
+    ok($.isFunction(this.constructor), 'should be a function');
+  });
+
+  module('instance', {
+    setup: function () {
+      this.instance = new $.Ninja.Slider('<input type="hidden"/>', {
+        list: ['one', 'two', 'three']
+      });
+    }
+  });
+
+  test('is an instance', function () {
+    ok(this.instance instanceof $.Ninja.Slider, 'should be instance of $.Ninja.Slider');
+  });
+
+  test('.$wrapper', function () {
+    ok(this.instance.$wrapper, 'should exist');
+    ok(this.instance.$wrapper.is('label'), 'should be a label element');
+    ok(this.instance.$wrapper.hasClass('ninja-slider'), 'should have slider class');
+  });
+
+  test('.list', function () {
+    ok(this.instance.list, 'should exist');
+    ok($.isArray(this.instance.list), 'should be an array');
+    strictEqual(this.instance.list[0], 'one', 'first element should match specified option');
+  });
+
+  test('.index', function () {
+    strictEqual(this.instance.index, 0, 'should equal 0');
+  });
+
+  module('elements', {
+    setup: function () {
+      this.$elements = $('#qunit-fixture').find('input');
+    }
+  });
+
+  test('are chainable', function () {
+    strictEqual(this.$elements.ninja('slider', {
+      list: []
+    }), this.$elements, 'should be chainable');
+  });
+
+  module('default', {
+    setup: function () {
+      this.$element = $('#qunit-fixture').find('input.test-default');
+
+      this.$element.ninja('slider', {
+        list: list
+      });
+    }
+  });
+
+  QUnit.done(function () {
+    $('#qunit-examples').find('input.test-default').ninja('slider', {
+      list: list
+    });
+  });
+
+  test('has wrapper', function () {
+    var $wrapper = this.$element.parent();
+
+    ok($wrapper.is('label'), 'should be a label element');
+
+    ok($wrapper.hasClass('ninja-slider'), 'wrapper have slider class');
+  });
+
+  module('value', {
+    setup: function () {
+      this.$element = $('#qunit-fixture').find('input.test-value');
+
+      this.$element.ninja('slider', {
+        list: list
+      });
+    }
+  });
+
+  QUnit.done(function () {
+    $('#qunit-examples').find('input.test-value').ninja('slider', {
+      list: list
+    });
+  });
+
+  test('has wrapper', function () {
+    var $wrapper = this.$element.parent();
+
+    ok($wrapper.is('label'), 'should be a label element');
+
+    ok($wrapper.hasClass('ninja-slider'), 'wrapper have slider class');
+  });
+
+  module('width', {
+    setup: function () {
+      this.$element = $('#qunit-fixture').find('input.test-width');
+
+      this.$element.ninja('slider', {
+        list: list
+      });
+    }
+  });
+
+  QUnit.done(function () {
+    $('#qunit-examples').find('input.test-width').ninja('slider', {
+      list: list
+    });
+  });
+
+  test('has wrapper', function () {
+    var $wrapper = this.$element.parent();
+
+    ok($wrapper.is('label'), 'should be a label element');
+
+    ok($wrapper.hasClass('ninja-slider'), 'wrapper have slider class');
+  });
+
+}(window.jQuery));
